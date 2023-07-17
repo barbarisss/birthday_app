d1d0867017d726c1d584c62a90ee449b1f755152
Vlad Edelev <vlladedellev@gmail.com>
2023-07-17 18:47:35 +0300

    feature: added colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const backgroundWhite = Color.fromRGBO(250, 251, 249, 1);
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const yellow = Color.fromRGBO(253, 172, 7, 1);
+  static const black = Color.fromRGBO(23, 16, 16, 1);
+  static const darkBlack = Color.fromRGBO(0, 0, 0, 1);
+  static const grey = Color.fromRGBO(78, 67, 67, 1);
+}
