805987d576234365197781574f5147088c6bfcda
Vlad Edelev <vlladedellev@gmail.com>
2023-07-17 19:15:09 +0300

    feature: added strings

---
+++ lib/utils/strings.dart
@@ -0,0 +1,23 @@
+abstract class AppStrings {
+  static const invite =
+      'Приглашаю своих дорогих друзей отметить мой день рождения в замечательном месте с множеством развлечений, вкусных блюд и хорошим настроением!';
+  static const guests = 'Список гостей';
+  static const wishlist = 'Вишлист';
+  static const menu = 'Меню';
+  static const canapes = 'Канапе';
+  static const cheesePlate = 'Сырная тарелка';
+  static const fruits = 'Свежие фрукты';
+  static const limonades = 'Авторские лимонады';
+  static const seafood = 'Морепродукты';
+  static const shashlik = 'Шашлык на мангале';
+  static const collapse = 'Свернуть';
+  static const entertainment = 'Развлечения';
+  static const boardGames = 'Настольные игры';
+  static const aboutBoardGames = 'Мафия, уно, домино, экивоки и другие';
+  static const pool = 'Бассейн';
+  static const aboutPool = 'Два бассейна с подогревом';
+  static const expand = 'Развернуть';
+  static const place = 'Место';
+  static const address = 'Центральная ул., 84, хутор Седых';
+  static const goToWebsite = 'Перейти на сайт места';
+}
