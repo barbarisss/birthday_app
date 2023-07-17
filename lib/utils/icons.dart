4416908ac5250ee9f0ad31f5cb77681900114f20
Vlad Edelev <vlladedellev@gmail.com>
2023-07-17 19:04:34 +0300

    feature: addded images & icons

---
+++ lib/utils/icons.dart
@@ -0,0 +1,4 @@
+abstract class AppIcons {
+  static const boardGames = 'assets/icons/board_games.svg';
+  static const pool = 'assets/icons/pool.svg';
+}
---
+++ lib/utils/images.dart
@@ -0,0 +1,10 @@
+abstract class AppImages {
+  static const camp = 'assets/images/camp.png';
+  static const canapes = 'assets/images/canapes.png';
+  static const cheesePlate = 'assets/images/cheese_plate.png';
+  static const fruits = 'assets/images/fruits.png';
+  static const limonades = 'assets/images/limonades.png';
+  static const seafood = 'assets/images/seafood.png';
+  static const shashlik = 'assets/images/shashlik.png';
+  static const map = 'assets/images/map.png';
+}
