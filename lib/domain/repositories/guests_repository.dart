0c7f4d47d3cdefd70c481a784b390c76a16154de
Vlad Edelev <vlladedellev@gmail.com>
2023-07-29 12:40:04 +0300

    refactor(guest_repository): rename param of add guest method

--- lib/domain/repositories/guests_repository.dart
+++ lib/domain/repositories/guests_repository.dart
@@ -2,5 +2,5 @@ import 'package:birthday_app/data/models/guest/guest_model.dart';
 
 abstract class GuestsRepository {
   Future<List<GuestModel>> getAllGuests();
-  Future<bool> addGuest(GuestModel task);
+  Future<bool> addGuest(GuestModel guestModel);
 }
