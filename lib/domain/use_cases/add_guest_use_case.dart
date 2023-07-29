a719b020a509b417592aebe3b86d0545e535e04a
Vlad Edelev <vlladedellev@gmail.com>
2023-07-29 12:38:52 +0300

    refactor: rename add_guest_use_case class

--- lib/domain/use_cases/add_guest_use_case.dart
+++ lib/domain/use_cases/add_guest_use_case.dart
@@ -1,8 +1,8 @@
 import 'package:birthday_app/data/models/guest/guest_model.dart';
 import 'package:birthday_app/domain/repositories/guests_repository.dart';
 
-class GetAllGuestsUseCase {
-  GetAllGuestsUseCase({required this.guestRepository});
+class AddGuestUseCase {
+  AddGuestUseCase({required this.guestRepository});
 
   final GuestsRepository guestRepository;
 
