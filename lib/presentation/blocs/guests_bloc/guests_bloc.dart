5c9b0521878c6e5baf3b05f02a5ad6bc12961713
Vlad Edelev <vlladedellev@gmail.com>
2023-08-02 14:22:36 +0300

    feature(guests_screen): provided guests bloc to the screen

--- lib/presentation/blocs/guests_bloc/guests_bloc.dart
+++ lib/presentation/blocs/guests_bloc/guests_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:birthday_app/core/utils/images.dart';
 import 'package:birthday_app/data/models/guest/guest_model.dart';
 import 'package:birthday_app/domain/use_cases/add_guest_use_case.dart';
 import 'package:birthday_app/domain/use_cases/get_all_guests_use_case.dart';
@@ -13,11 +14,43 @@ class GuestsBloc extends Bloc<GuestsEvent, GuestsState> {
     required this.getAllGuestsUseCase,
     required this.addGuestUseCase,
   }) : super(const GuestsState.initial()) {
-    on<GuestsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<GetAllGuestsEvent>(_onGetAllGuests);
   }
 
   final GetAllGuestsUseCase getAllGuestsUseCase;
   final AddGuestUseCase addGuestUseCase;
+
+  _onGetAllGuests(GetAllGuestsEvent event, Emitter<GuestsState> emit) async {
+    emit(const GuestsState.loading());
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<GuestModel> guests = [
+      GuestModel(
+        avatar: AppImages.avatar,
+        name: 'Иван',
+        surname: 'Иванов',
+        birthDate: DateTime.now(),
+        phoneNumber: '+7 999 999 99 99',
+        profession: 'Сварщик',
+      ),
+      GuestModel(
+        avatar: AppImages.avatar,
+        name: 'Иван',
+        surname: 'Иванов',
+        birthDate: DateTime.now(),
+        phoneNumber: '+7 999 999 99 99',
+        profession: 'Сварщик',
+      ),
+      GuestModel(
+        avatar: AppImages.avatar,
+        name: 'Иван',
+        surname: 'Иванов',
+        birthDate: DateTime.now(),
+        phoneNumber: '+7 999 999 99 99',
+        profession: 'Сварщик',
+      ),
+    ];
+
+    emit(GuestsState.loaded(guests));
+  }
 }
--- lib/presentation/screens/guests/guests_screen.dart
+++ lib/presentation/screens/guests/guests_screen.dart
@@ -1,14 +1,19 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:birthday_app/app/di/injector.dart';
 import 'package:birthday_app/core/utils/colors.dart';
 import 'package:birthday_app/core/utils/constants.dart';
 import 'package:birthday_app/core/utils/images.dart';
 import 'package:birthday_app/core/utils/strings.dart';
 import 'package:birthday_app/data/models/guest/guest_model.dart';
+import 'package:birthday_app/presentation/blocs/guests_bloc/guests_bloc.dart';
 import 'package:birthday_app/presentation/screens/guests/widgets/guest_list.dart';
 import 'package:birthday_app/presentation/screens/guests/widgets/guest_modal_bottom_sheet.dart';
 import 'package:birthday_app/presentation/shared_widgets/custom_floating_action_button.dart';
 import 'package:birthday_app/presentation/shared_widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class GuestsScreenWidget extends StatelessWidget {
@@ -16,55 +21,52 @@ class GuestsScreenWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<GuestModel> guests = [
-      GuestModel(
-        avatar: AppImages.avatar,
-        name: 'Иван',
-        surname: 'Иванов',
-        birthDate: DateTime.now(),
-        phoneNumber: '+7 999 999 99 99',
-        profession: 'Сварщик',
-      ),
-      GuestModel(
-        avatar: AppImages.avatar,
-        name: 'Иван',
-        surname: 'Иванов',
-        birthDate: DateTime.now(),
-        phoneNumber: '+7 999 999 99 99',
-        profession: 'Сварщик',
-      ),
-      GuestModel(
-        avatar: AppImages.avatar,
-        name: 'Иван',
-        surname: 'Иванов',
-        birthDate: DateTime.now(),
-        phoneNumber: '+7 999 999 99 99',
-        profession: 'Сварщик',
-      ),
-    ];
+    late Widget bodyWidget;
 
-    return Scaffold(
-      appBar: const MainAppBarWidget(title: AppStrings.guests),
-      floatingActionButton: CustomFloatingActionButton(
-        onPressed: () {
-          final guestModalBottomSheet = GuestModalBottomSheet(
-            context: context,
-            onButtonPressed: () {},
-          );
-          guestModalBottomSheet.show();
-        },
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: AppConstants.mainPaddingWidth,
+    return BlocProvider(
+      create: (context) =>
+          injector<GuestsBloc>()..add(const GetAllGuestsEvent()),
+      child: Scaffold(
+        appBar: const MainAppBarWidget(title: AppStrings.guests),
+        floatingActionButton: CustomFloatingActionButton(
+          onPressed: () {
+            final guestModalBottomSheet = GuestModalBottomSheet(
+              context: context,
+              onButtonPressed: () {},
+            );
+            guestModalBottomSheet.show();
+          },
         ),
-        child: Column(
-          children: [
-            SizedBox(height: AppConstants.mainPaddingHeight),
-            Expanded(
-              child: GuestListWidget(guests: guests),
-            )
-          ],
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: AppConstants.mainPaddingWidth,
+          ),
+          child: BlocBuilder<GuestsBloc, GuestsState>(
+            builder: (context, state) {
+              state.when(
+                initial: () {
+                  bodyWidget = const SizedBox();
+                },
+                loading: () {
+                  bodyWidget = const Center(
+                      child: CircularProgressIndicator(
+                    color: AppColors.green,
+                  ));
+                },
+                loaded: (guests) {
+                  bodyWidget = Column(
+                    children: [
+                      SizedBox(height: AppConstants.mainPaddingHeight),
+                      Expanded(
+                        child: GuestListWidget(guests: guests),
+                      )
+                    ],
+                  );
+                },
+              );
+              return bodyWidget;
+            },
+          ),
         ),
       ),
     );
